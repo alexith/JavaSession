4362d49b3a170d0d5e117baa16e6936b4c8b995a
home <akshay.cy2@gmail.com>
2020-09-27 22:12:19 -0400

    Bank Account

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,26 @@ public class Main{
 
         BankAccount akshayBankAccount = new BankAccount();
 
+        System.out.println("Please enter the account number: ");
         String myNewAccount = akshayBankAccount.accountNumber = scanner.nextLine();
 
-        System.out.println(myNewAccount);
+        System.out.println("Please, enter the name: ");
+        String myName = akshayBankAccount.customerName = scanner.nextLine();
+
+        System.out.println("Hello, " + myName);
+        System.out.println("Your entered account no. : " + myNewAccount);
+
+
+        System.out.println("Do you want to (1)withdraw or (2)deposit ? (1/2) ");
+        String withdrawOrDeposit = scanner.nextLine();
+
+        if(withdrawOrDeposit.equals("1")) {
+            System.out.println("Please enter the amount you want to withdraw: ");
+            Double withdrawAmount = akshayBankAccount.currentAmount = scanner.nextDouble();
+        } else if(withdrawOrDeposit.equals("2")){
+            System.out.println("Please enter the amount you want to deposit: ");
+            Double depositAmount = akshayBankAccount.currentAmount = scanner.nextDouble();
+        }
 
         scanner.close();
 
